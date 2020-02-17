f35af461d79953552b1b2d50ada52734311f702a
Tracie T <traciecodes@gmail.com>
2020-02-17 12:49:06 -0600

    Added basic routing setup with CRUD endpoints

---
+++ routes/bootcamps.routes.js
@@ -0,0 +1,35 @@
+const express = require('express');
+const router = express.Router();
+
+// GET all bootcamps
+router.get('/', (req, res) => {
+	res.status(200).json({ success: true, msg: `This will show all bootcamps.` });
+});
+
+// GET a specific bootcamp by id
+router.get('/:id', (req, res) => {
+	res
+		.status(200)
+		.json({ success: true, msg: `This will show Bootcamp #${req.params.id}` });
+});
+
+// POST to create new bootcamp
+router.post('/', (req, res) => {
+	res.status(200).json({ success: true, msg: `Created a new bootcamp!` });
+});
+
+// PUT to update existing bootcamp
+router.put('/:id', (req, res) => {
+	res
+		.status(200)
+		.json({ success: true, msg: `Updated Bootcamp #${req.params.id}` });
+});
+
+// DELETE a bootcamp
+router.delete('/:id', (req, res) => {
+	res
+		.status(200)
+		.json({ success: true, msg: `Deleted Bootcamp #${req.params.id}!` });
+});
+
+module.exports = router;
--- server.js
+++ server.js
@@ -1,10 +1,15 @@
 const express = require('express');
 const dotenv = require('dotenv');
 
+// route files
+const bootcamps = require('./routes/bootcamps.routes');
 // load env vars
 dotenv.config({ path: './config/config.env' });
 
 const app = express();
+
+// mount routers
+app.use('/api/v1/bootcamps', bootcamps);
 
 const PORT = process.env.PORT || 7000;
 
