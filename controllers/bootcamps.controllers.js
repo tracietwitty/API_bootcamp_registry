e98d353bdbb8c46589adfee006ab24dcc1558a9c
Tracie Twite <traciecodes@gmail.com>
2020-02-20 11:34:15 -0600

    Set up mongoose-specific error handling and implemented it for all CRUD endpoints

--- controllers/bootcamps.controllers.js
+++ controllers/bootcamps.controllers.js
@@ -13,7 +13,7 @@ exports.getBootcamps = async (req, res, next) => {
 			.status(200)
 			.json({ success: true, count: bootcamps.length, data: bootcamps });
 	} catch (err) {
-		res.status(400).json({ success: false });
+		next(err);
 	}
 };
 
@@ -32,9 +32,7 @@ exports.getBootcamp = async (req, res, next) => {
 
 		res.status(200).json({ success: true, data: bootcamp });
 	} catch (err) {
-		next(
-			new ErrorResponse(`Bootcamp not found with id of ${req.params.id}`, 404)
-		);
+		next(err);
 	}
 };
 
@@ -50,7 +48,7 @@ exports.createBootcamp = async (req, res, next) => {
 			data: bootcamp
 		});
 	} catch (err) {
-		res.status(400).json({ success: false });
+		next(err);
 	}
 };
 
@@ -65,12 +63,14 @@ exports.updateBootcamp = async (req, res, next) => {
 		});
 
 		if (!bootcamp) {
-			return res.status(400).json({ success: false });
+			return next(
+				new ErrorResponse(`Bootcamp not found with id of ${req.params.id}`, 404)
+			);
 		}
 
 		res.status(200).json({ success: true, data: bootcamp });
 	} catch (err) {
-		res.status(400).json({ success: false });
+		next(err);
 	}
 };
 
@@ -82,11 +82,13 @@ exports.deleteBootcamp = async (req, res, next) => {
 		const bootcamp = await Bootcamp.findByIdAndDelete(req.params.id);
 
 		if (!bootcamp) {
-			return res.status(400).json({ success: false });
+			return next(
+				new ErrorResponse(`Bootcamp not found with id of ${req.params.id}`, 404)
+			);
 		}
 
 		res.status(200).json({ success: true, data: {} });
 	} catch (err) {
-		res.status(400).json({ success: false });
+		next(err);
 	}
 };
