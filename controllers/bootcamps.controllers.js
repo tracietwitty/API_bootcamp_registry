3e82d5dacf6f85152b1639145e2d9435df58c4c5
Tracie Twite <traciecodes@gmail.com>
2020-02-24 17:18:34 -0600

    Used cascade delete method to delete all courses assoc. with a single bootcamp when that bootcamp is removed

--- controllers/bootcamps.controllers.js
+++ controllers/bootcamps.controllers.js
@@ -30,7 +30,7 @@ exports.getBootcamps = asyncHandler(async (req, res, next) => {
 	);
 
 	// Find resource:
-	query = Bootcamp.find(JSON.parse(queryString));
+	query = Bootcamp.find(JSON.parse(queryString)).populate('courses');
 
 	// Select specific fields:
 	if (req.query.select) {
@@ -48,7 +48,7 @@ exports.getBootcamps = asyncHandler(async (req, res, next) => {
 
 	// Set up pagination with page 1 and 25 results showing per page as default:
 	const page = parseInt(req.query.page, 10) || 1;
-	const limit = parseInt(req.query.limit, 10) || 1;
+	const limit = parseInt(req.query.limit, 10) || 25;
 	const startIndex = (page - 1) * limit;
 	const endIndex = page * limit;
 	const total = await Bootcamp.countDocuments();
@@ -129,13 +129,15 @@ exports.updateBootcamp = asyncHandler(async (req, res, next) => {
 // @route           DELETE /api/v1/bootcamps/:id
 // @access          Private
 exports.deleteBootcamp = asyncHandler(async (req, res, next) => {
-	const bootcamp = await Bootcamp.findByIdAndDelete(req.params.id);
+	const bootcamp = await Bootcamp.findById(req.params.id);
 
 	if (!bootcamp) {
 		return next(
 			new ErrorResponse(`Bootcamp not found with id of ${req.params.id}`, 404)
 		);
 	}
+
+	bootcamp.remove();
 
 	res.status(200).json({ success: true, data: {} });
 });
